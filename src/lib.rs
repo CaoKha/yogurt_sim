88b84c2604ea2eb375223d6d5637cd13bc35a68e
Steve Nguyen <nck13@pop-os.localdomain>
2022-11-26 19:52:05 +0100

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
+#![allow(non_snake_case)]
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
 
 use winit::{
-    dpi::PhysicalSize,
+    // dpi::PhysicalSize,
     event::*,
     event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
+    // window::Window,
 };
 
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
@@ -58,3 +60,46 @@ pub fn run() {
         _ => {}
     })
 }
+
+// struct State {
+//     surface: wgpu::Surface,
+//     device: wgpu::Device,
+//     queue: wgpu::Queue,
+//     config: wgpu::SurfaceConfiguration,
+//     size: winit::dpi::PhysicalSize<u32>,
+// }
+
+// impl State {
+//     // Creating some of the wgpu types requires async code
+//     async fn new(window: &Window) -> Self {
+//         let size = window.inner_size();
+//         let instance = wgpu::Instance::new(wgpu::Backend::all());
+//         let surface = unsafe {
+//             instance.create_surface(window)
+//         };
+//         let adapter = instance.request_adapter(
+//             &wgpu::RequestAdapterOptions {
+//                 power_preference: wgpu::PowerPreference::default(),
+//                 compatible_surface: Some(&surface),
+//                 force_fallback_adapter: false,
+//             }
+//         ).await.unwrap();
+//     }
+
+//     fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+//         todo!()
+//     }
+
+//     fn input(&mut self, event: &WindowEvent) -> bool {
+//         todo!()
+//     }
+
+//     fn update(&mut self) {
+//         todo!()
+//     }
+
+//     fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+//         todo!()
+//     }
+// }
+
