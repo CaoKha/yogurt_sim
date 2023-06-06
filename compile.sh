5fdc56373b2016c26d04efdf3966058da46e59d2
CaoKha <nguyencaokha131995@gmail.com>
2023-06-06 15:45:12 +0200

    update compile.sh if it is ran from a different directory

--- compile.sh
+++ compile.sh
@@ -3,4 +3,6 @@
 # compile rust to wasm
 # cargo build --release --target wasm32-unknown-unknown
 # wasm-bindgen --out-dir ./server/public/wasm/ --target web ./target/wasm32-unknown-unknown/release/yogurt_sim.wasm
-wasm-pack build . --release --target web
+BASEDIR=$(dirname "$0")
+echo "$BASEDIR"
+wasm-pack build $BASEDIR --release --target web
