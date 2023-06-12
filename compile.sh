7d978525a67f3009a51fdf01cd9a7792c6f1d8e2
CaoKha <nguyencaokha131995@gmail.com>
2023-06-12 17:55:48 +0200

    fix build.sh

--- compile.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-#
-# compile rust to wasm
-# cargo build --release --target wasm32-unknown-unknown
-# wasm-bindgen --out-dir ./server/public/wasm/ --target web ./target/wasm32-unknown-unknown/release/yogurt_sim.wasm
-BASEDIR=$(dirname "$0")
-echo "$BASEDIR"
-wasm-pack build $BASEDIR --release --target web --out-dir server/pkg
